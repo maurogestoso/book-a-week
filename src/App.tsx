d6aaed237b395b46aea336ac6b0f5d52f8ae52dd
maurogestoso <maurogestoso@gmail.com>
2025-10-20 01:20:40 +0200

    Center layout

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import {
   FieldSet,
 } from "./components/ui/field";
 import { useState } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "./components/ui/card";
 
 function App() {
   const [books, setBooks] = useState<Book[]>([]);
@@ -19,15 +20,23 @@ function App() {
 
   return (
     <>
-      <Header />
-      <main className="mt-4">
-        <AddBookForm onSubmit={addBook} />
-        <ol>
-          {books.map((book) => (
-            <li key={book.title}>{book.title}</li>
-          ))}
-        </ol>
-      </main>
+      <div className="max-w-md mx-auto">
+        <Header />
+        <main className="mt-4">
+          <AddBookForm onSubmit={addBook} />
+          <div className="mt-8">
+            <h2 className="text-2xl font-bold mb-4">Books:</h2>
+            {books.map((book) => (
+              <Card key={book.title}>
+                <CardHeader>
+                  <CardTitle>{book.title}</CardTitle>
+                </CardHeader>
+                <CardContent></CardContent>
+              </Card>
+            ))}
+          </div>
+        </main>
+      </div>
     </>
   );
 }
@@ -51,58 +60,54 @@ type Book = {
 
 function AddBookForm({ onSubmit }: { onSubmit: (book: Book) => void }) {
   return (
-    <div className="w-full max-w-md">
-      <form
-        onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
-          e.preventDefault();
-          const form = e.currentTarget;
-          const formData = new FormData(form);
-          const book: Book = {
-            title: formData.get("title") as string,
-            totalPages: parseInt(formData.get("totalPages") as string),
-            daysToFinish: parseInt(formData.get("daysToFinish") as string),
-            startDate: new Date(),
-          };
-          onSubmit(book);
-          form.reset();
-        }}
-      >
-        <FieldGroup>
-          <FieldSet>
-            <FieldLegend>Add book</FieldLegend>
-            <FieldGroup>
-              <Field>
-                <FieldLabel htmlFor="title">Title:</FieldLabel>
-                <Input type="text" id="title" name="title" autoFocus required />
-              </Field>
-              <Field>
-                <FieldLabel htmlFor="total-pages">Total pages:</FieldLabel>
-                <Input
-                  type="number"
-                  id="total-pages"
-                  name="totalPages"
-                  required
-                />
-              </Field>
-              <Field>
-                <FieldLabel htmlFor="days-to-finish">
-                  Days to finish:
-                </FieldLabel>
-                <Input
-                  type="number"
-                  defaultValue={7}
-                  id="days-to-finish"
-                  name="daysToFinish"
-                  required
-                />
-              </Field>
-            </FieldGroup>
-          </FieldSet>
-          <Field>
-            <Input type="submit" value="Add" />
-          </Field>
-        </FieldGroup>
-      </form>
-    </div>
+    <form
+      onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const form = e.currentTarget;
+        const formData = new FormData(form);
+        const book: Book = {
+          title: formData.get("title") as string,
+          totalPages: parseInt(formData.get("totalPages") as string),
+          daysToFinish: parseInt(formData.get("daysToFinish") as string),
+          startDate: new Date(),
+        };
+        onSubmit(book);
+        form.reset();
+      }}
+    >
+      <FieldGroup>
+        <FieldSet>
+          <FieldLegend>Add book</FieldLegend>
+          <FieldGroup>
+            <Field>
+              <FieldLabel htmlFor="title">Title:</FieldLabel>
+              <Input type="text" id="title" name="title" autoFocus required />
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="total-pages">Total pages:</FieldLabel>
+              <Input
+                type="number"
+                id="total-pages"
+                name="totalPages"
+                required
+              />
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="days-to-finish">Days to finish:</FieldLabel>
+              <Input
+                type="number"
+                defaultValue={7}
+                id="days-to-finish"
+                name="daysToFinish"
+                required
+              />
+            </Field>
+          </FieldGroup>
+        </FieldSet>
+        <Field>
+          <Input type="submit" value="Add" />
+        </Field>
+      </FieldGroup>
+    </form>
   );
 }
